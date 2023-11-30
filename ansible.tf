0be6020371bdac962634328bb8a5e1c30cea84f3
tsisar <fhantom.tp@gmail.com>
2023-11-30 22:26:54 +0200

    refactoring

--- ansible.tf
+++ ansible.tf
@@ -1,17 +1,21 @@
 resource "local_file" "ansible_inventory" {
   filename = "./ansible/inventory.ini"
   content     = <<-EOF
-  k8s-cp-01 ansible_host=${proxmox_virtual_environment_vm.k8s_cp_01.ipv4_addresses[1][0]} ansible_become=true
+  k8s-master-01 ansible_host=${proxmox_virtual_environment_vm.k8s_master_01.ipv4_addresses[1][0]} ansible_become=true
   k8s-worker-01 ansible_host=${proxmox_virtual_environment_vm.k8s_worker_01.ipv4_addresses[1][0]} ansible_become=true
+  k8s-worker-02 ansible_host=${proxmox_virtual_environment_vm.k8s_worker_02.ipv4_addresses[1][0]} ansible_become=true
+  k8s-worker-03 ansible_host=${proxmox_virtual_environment_vm.k8s_worker_03.ipv4_addresses[1][0]} ansible_become=true
 
   [kube_control_plane]
-  k8s-cp-01
+  k8s-master-01
 
   [etcd]
-  k8s-cp-01
+  k8s-master-01
 
   [kube_node]
   k8s-worker-01
+  k8s-worker-02
+  k8s-worker-03
 
   [k8s_cluster:children]
   kube_node
