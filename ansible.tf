b6abe7f00b7d534e48210b557c13959d4d909ce4
tsisar <fhantom.tp@gmail.com>
2023-11-30 19:19:53 +0200

    refactoring

--- ansible.tf
+++ ansible.tf
@@ -3,15 +3,20 @@ resource "local_file" "ansible_inventory" {
   content     = <<-EOF
   k8s-cp-01 ansible_host=${proxmox_virtual_environment_vm.k8s_cp_01.ipv4_addresses[1][0]} ansible_become=true
   k8s-worker-01 ansible_host=${proxmox_virtual_environment_vm.k8s_worker_01.ipv4_addresses[1][0]} ansible_become=true
+
   [kube_control_plane]
   k8s-cp-01
+
   [etcd]
   k8s-cp-01
+
   [kube_node]
   k8s-worker-01
+
   [k8s_cluster:children]
   kube_node
   kube_control_plane
+
   EOF
 
   provisioner "local-exec" {
